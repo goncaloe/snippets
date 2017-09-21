37266da5a09ad40fff163b9e02b46ee4f1691c70
Gonçalo Esteves <goncalo.p.esteves@gmail.com>
2017-09-21 14:04:26 +0100

    local config is optional

--- web/index.php
+++ web/index.php
@@ -8,9 +8,9 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require(APP_BASE_PATH . '/vendor/autoload.php');
 require(APP_BASE_PATH . '/app/Yii.php');
 
-$config = yii\helpers\ArrayHelper::merge(
-    require(APP_BASE_PATH . '/app/config/web.php'),
-    require(APP_BASE_PATH . '/app/config/local.php')
-);
+$config = require(APP_BASE_PATH . '/app/config/web.php');
+if(file_exists(APP_BASE_PATH . '/app/config/local.php')){
+    $config = yii\helpers\ArrayHelper::merge($config, require(APP_BASE_PATH . '/app/config/local.php'));
+}
 
 (new app\Application($config))->run();
